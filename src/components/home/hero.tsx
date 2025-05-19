cadeeb12cf34dd035429944e0b654096aa1d4f01
Blankeye-zero <60312301+Blankeye-zero@users.noreply.github.com>
2025-05-19 12:16:42 +0530

    added enquiry section

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -217,6 +217,99 @@ const HomeHero: FC = () => {
             ))}
           </Grid>
         </Box>
+        <Box sx={{display:"flex", flexDirection: {xs: 'column', md: 'row-reverse'}, mt: 10,}}>
+      <Container sx={{ display: 'flex', justifyContent: {xs: 'center', md: 'start'}}}>
+         <Box sx={{ mb: 3 }}>
+                <Typography
+                  component="h2"
+                  sx={{
+                    position: 'relative',
+                    fontSize: { xs: 40, md: 72 },
+                    letterSpacing: 1.5,
+                    fontWeight: 'bold',
+                    lineHeight: 1.3,
+                  }}
+                >
+                  <Typography
+                    component="mark"
+                    sx={{
+                      position: 'relative',
+                      color: 'primary.main',
+                      fontSize: 'inherit',
+                      fontWeight: 'inherit',
+                      backgroundColor: 'unset',
+                    }}
+                  >
+                    100%{' '}
+                    <Box
+                      sx={{
+                        position: 'absolute',
+                        top: { xs: 24, md: 34 },
+                        left: 2,
+                        transform: 'rotate(3deg)',
+                        '& img': { width: { xs: 146, md: 210 }, height: 'auto' },
+                      }}
+                    >
+                      {/* eslint-disable-next-line */}
+                      <img src="/images/headline-curve.svg" alt="Headline curve" />
+                    </Box>
+                  </Typography>
+                  {' '}focus on{' '}
+                  <Typography
+                    component="span"
+                    sx={{
+                      fontSize: 'inherit',
+                      fontWeight: 'inherit',
+                      position: 'relative',
+                      '& svg': {
+                        position: 'absolute',
+                        top: -16,
+                        right: -21,
+                        width: { xs: 22, md: 30 },
+                        height: 'auto',
+                      },
+                    }}
+                  >
+                    Islamic Principles
+                    <svg version="1.1" viewBox="0 0 3183 3072">
+                      <g id="Layer_x0020_1">
+                        <path
+                          fill="#127C71"
+                          d="M2600 224c0,0 0,0 0,0 236,198 259,562 52,809 -254,303 -1849,2089 -2221,1776 -301,-190 917,-1964 1363,-2496 207,-247 570,-287 806,-89z"
+                        />
+                        <path
+                          fill="#127C71"
+                          d="M3166 2190c0,0 0,0 0,0 64,210 -58,443 -270,516 -260,90 -1848,585 -1948,252 -104,-230 1262,-860 1718,-1018 212,-73 437,39 500,250z"
+                        />
+                        <path
+                          fill="#127C71"
+                          d="M566 3c0,0 0,0 0,0 -219,-26 -427,134 -462,356 -44,271 -255,1921 90,1962 245,62 628,-1392 704,-1869 36,-221 -114,-424 -332,-449z"
+                        />
+                      </g>
+                    </svg>
+                  </Typography>{' '}
+                  <br />
+                </Typography>
+                <Box sx={{ mb: 4, mt:5, width: { xs: '100%', md: '100%' } }}>
+                <Typography sx={{ color: 'text.secondary', lineHeight: 1.6,fontSize:28 }}>
+                  {
+                    "Need Help? We are just a call away" 
+                  }
+                  <br/>
+                  {
+                    "9962786863"
+                  }
+                  <br />
+                  {
+                    "9884595100"
+                  }
+                </Typography>
+                  <img src="/images/ukg.jpg"  alt="Hero img" style={{borderRadius: '20px',objectFit:'cover', width:'min(100%, 750px)', height:'686px'}}  />
+              </Box>
+              </Box>
+      </Container>
+      <Container sx={{width: "100%", display:'flex', justifyContent: {xs: 'center', md:'end'}}}><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdEYgHFtGMuYm56e_GCYG0au-WlrXyp6aHl5FjGbJ7PXR8pIA/viewform?embedded=true" width="640" height="1300" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe></Container>
+      </Box>
       </Container>
     </Box>
   )
